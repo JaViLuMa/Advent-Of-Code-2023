8ae1dfb7f02da250ac619e31e2d9c30d126a26b4
Matej Ruzic <matej.gamer01@gmail.com>
2023-12-15 13:53:54 +0100

    small magic number separation

--- day_15/code.py
+++ day_15/code.py
@@ -1,3 +1,6 @@
+BOXES_RANGE = 256
+
+
 LENSES = dict[str, int]
 BOXES = dict[int, LENSES]
 
@@ -12,7 +15,7 @@ def HASH(sequence: str) -> int:
 
 
 def HASHMAP(sequences: list[str]) -> BOXES:
-    boxes: BOXES = {box: {} for box in range(256)}
+    boxes: BOXES = {box: {} for box in range(BOXES_RANGE)}
 
     for sequence in sequences:
         if "=" in sequence:
@@ -43,7 +46,7 @@ total_HASH_sum: int = sum(HASH(sequence) for sequence in initial_sequence)
 populated_boxes = HASHMAP(initial_sequence)
 
 total_HASHMAP_sum: int = sum(
-    specific_box_sum(populated_boxes[box], box) for box in range(256)
+    specific_box_sum(populated_boxes[box], box) for box in range(BOXES_RANGE)
 )
 
 print(f"Part 1: {total_HASH_sum} | Part 2: {total_HASHMAP_sum}")
